2afba10a8b8f751cccb9b5d896a53a8918b54712
axeltav <axelgit2@gmail.com>
2024-10-14 20:59:58 +0100

    Commit teste

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    res.json({ status: "Funfando" });
+    res.json({ status: "Funfando teste teste teste" });
 })
 
 export { router };
