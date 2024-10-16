22ac5480b9ea8c4eef99768406961d927baf87ec
Eliseu Silva <eliseusilva.info@gmail.com>
2024-10-16 17:44:41 +0100

    Refactor CreateUserService to handle user creation and validation

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma'
+
 interface UserRequest {
     name: string;
     email: string;
@@ -6,9 +8,38 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({name, email, password}: UserRequest) {
-        console.log(name);
+        
+        // verificar se ele enviou um email
+        if(!email) {
+            throw new Error("Email incorreto.");
+        }
 
-        return { name: name }
+        // verificar se esse email já está cadastrado na plataforma
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        });
+
+        if(userAlreadyExists) {
+            throw new Error("Usuário já existe.");
+        }
+
+        // criar usuário
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true
+            }
+        });
+
+        return user;
     }
 }
 
